ee68873752113556d45f0fe6583298aefba9918f
Yask Srivastava <yask123@gmail.com>
2016-02-07 16:12:52 +0530

    Project Euler problem 31

--- coin_change_problem.py
+++ coin_change_problem.py
@@ -2,10 +2,11 @@
 Given a value N, if we want to make change for N cents, and we have infinite supply of each of
 S = { S1, S2, .. , Sm}  valued coins,
 how many ways can we make the change?
+https://projecteuler.net/problem=31
 '''
 
 
-def count(list_of_coins, m, change):
+def count(list_of_coins, m, change, cache):
     if change == 0:
         return 1
 
@@ -14,8 +15,9 @@ def count(list_of_coins, m, change):
 
     if m < 0:
         return 0
-    print change
-    return count(list_of_coins, m - 1, change - list_of_coins[m]) + count(list_of_coins, m - 1, change)
+    cache[change] = count(list_of_coins, m, change - list_of_coins[m], cache) + count(list_of_coins, m - 1, change,
+                                                                                      cache)
+    return cache[change]
 
 
-print count([1, 2, 3], 2, 4)
+print count([1, 2, 5, 10, 20, 50, 100, 200], 7, 200, {})
