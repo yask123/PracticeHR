20d751e87eb57a12811d020b39626b60fd298497
Yask Srivastava <yask123@gmail.com>
2016-02-07 14:31:44 +0530

    Memozied solution of coin DP

--- coinsdp.py
+++ coinsdp.py
@@ -1,16 +1,20 @@
-def min_coins(coin_value_list, change):
+def min_coins(coin_value_list, change, cache):
     minCoins = change
 
     if change in coin_value_list:
+        cache[change] = 1
         return 1
+    elif cache[change] > 0:
+        return cache[change]
     else:
 
         for i in [c for c in coin_value_list if c <= change]:
-            numCoins = 1 + min_coins(coin_value_list, change - i)
+            numCoins = 1 + min_coins(coin_value_list, change - i, cache)
 
             if numCoins < minCoins:
                 minCoins = numCoins
+                cache[change] = minCoins
         return minCoins
 
 
-print min_coins([1, 5], 10)
+print min_coins([1, 5, 10, 25], 63, [0] * 64)
