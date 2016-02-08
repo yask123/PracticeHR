789b0e53806362665d7b30f70d46a090cc40ba74
Yask Srivastava <yask123@gmail.com>
2016-02-08 15:50:48 +0530

    Binary heap! <3

---
+++ MyHeap.py
@@ -0,0 +1,38 @@
+class Binheap:
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i // 2]:
+                self.heapList[i], self.heapList[i // 2] = self.heapList[i // 2], self.heapList[i]
+            i = i // 2
+
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize += 1
+        self.percUp(self.currentSize)
+
+    def percDown(self, i):
+        while i * 2 <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                self.heapList[mc], self.heapList[i] = self.heapList[i], self.heapList[mc]
+            i = mc
+
+    def minChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def delMin(self):
+        retVal = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize -= 1
+        self.heapList.pop()
+        self.percDown(1)
