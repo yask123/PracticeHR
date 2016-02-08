f432844c2657736dd827069c18cca23cbcd0ca3c
Yask Srivastava <yask123@gmail.com>
2016-02-08 13:10:22 +0530

    string compression

--- string_compress.py
+++ string_compress.py
@@ -15,14 +15,11 @@ for i in range(1, len(alist)):
         count += 1
 
     else:
-        print prev, count,
-        print 'first char index ', first_char_index
         alist[first_char_index] = prev + str(count)
         count = 1
         prev = current
         first_char_index = i
 alist[first_char_index] = prev + str(count)
-print ''
 
 for each in alist:
     if each != '':
