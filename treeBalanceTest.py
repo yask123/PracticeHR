20bc7e45c13c9cf2f595929837fa2f98d966214e
Yask Srivastava <yask123@gmail.com>
2016-02-10 14:46:51 +0530

    Is tree balanced ?

---
+++ treeBalanceTest.py
@@ -0,0 +1,23 @@
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    ans = 1
+
+    def findDepth(self, node, depth):
+        if (node == None):
+            return depth - 1
+        lt_dt = self.findDepth(node.left, depth + 1)
+        rt_dt = self.findDepth(node.right, depth + 1)
+        if (abs(lt_dt - rt_dt) > 1):
+            self.ans = 0
+        return max(lt_dt, rt_dt)
+
+    def isBalanced(self, A):
+        self.findDepth(A, 0)
+        return self.ansself.tree_height(A.right, height + 1))
